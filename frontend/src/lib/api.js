903a6e7ff94ece0d1517226c1bae8536a465a362
Emrys <brunokiprono@gmail.com>
2026-09-04 23:34:26 +1000

    Fix production API URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -4,7 +4,7 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API = `${BACKEND_URL}/api`;
 
 const api = axios.create({
-  baseURL:"http://localhost:8000/api",
+  baseURL: API,
   withCredentials: true,
 });
 
